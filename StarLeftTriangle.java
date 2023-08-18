8006e7de721ade01867e69bdbc2e88fd42470e6b
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-08-19 01:01:05 +0530

    starpattern

---
+++ StarLeftTriangle.java
@@ -0,0 +1,15 @@
+public class StarLeftTriangle {
+    public static void main(String[] args) {
+        
+    
+    for(int i=1 ; i<=4 ; i++){
+        for(int j=4 ; j>=i ; j--){
+            System.out.print("");
+        }
+        for(int k=1 ; k<=i ; k++){
+            System.out.print("*");
+        }
+        System.out.println("");
+}
+}
+}
