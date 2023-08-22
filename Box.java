6e2f5fad4ef98a4e75927f2c00eba42deaf996c5
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-08-22 23:02:09 +0530

    add

---
+++ Box.java
@@ -0,0 +1,15 @@
+public class Box {
+    public static void main(String[] args)  {
+        pattern1(n:4);
+    }
+    static void pattern1(int n){
+            for(int row =1 ; row<=n ; row++) {
+                //for every row ; run col
+                for(int col =1 ; col <= n ; col++){
+                    System.out.print("* ");
+                }
+                //when one row is printed , weadd newline
+                System.out.println();
+            }
+        }
+    }
