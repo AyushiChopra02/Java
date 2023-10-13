b2b8ba39c1a130816f616158ee97e18a86285980
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-10-13 23:54:51 +0530

    add

--- Cross.java
+++ Cross.java
@@ -6,7 +6,7 @@ public class Cross {
                     System.out.print("*");
                 }
                 else{
-                    System.out.print(" ");
+                    System.out.print(" ") ;
                 }
                 }
             
