c06843d33c10462f2e54a2ac9b477e9a35894b76
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-10-14 23:18:22 +0530

    add

--- StarRightTriangle.java
+++ StarRightTriangle.java
@@ -1,7 +1,7 @@
 public class StarRightTriangle{
     public static void main(String[] args) {
         
-                for( int i=1 ; i<=4 ; i++){
+                for(int i=1 ; i<=4 ; i++){
                     for(int j=1 ; j<=i ; j++){
                         System.out.print("*");
                     }
