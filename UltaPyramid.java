40bf67f373b9a5ab406ef1995b869e3c8b7858cf
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-10-14 23:19:30 +0530

    add

--- UltaPyramid.java
+++ UltaPyramid.java
@@ -12,7 +12,7 @@ public class UltaPyramid {
             for(int l=3 ; l>=i ; l--){
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println() ;
         }
     }
     
