1c94a2b1f2546f05542e1088d49a237e1d00ce14
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-10-15 19:35:16 +0530

    add

--- UltaPyramid.java
+++ UltaPyramid.java
@@ -1,6 +1,6 @@
     
 
-public class UltaPyramid {
+public class UltaPyramid  {
     public static void main(String[] args) {
         for(int i=1 ; i<=4 ; i++){
             for(int j=1 ; j<=i ; j++){
