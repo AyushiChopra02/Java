55ac7b49e7edeb3a496d20b6930d36d2d07b6a88
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-10-15 19:35:56 +0530

    add

--- Pyramid.java
+++ Pyramid.java
@@ -1,5 +1,5 @@
 public class Pyramid {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         for(int i=1 ; i<=4 ; i++){
             for(int j=4 ; j>=i ; j--){
                 System.out.print("");
